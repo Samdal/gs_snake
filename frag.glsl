7434263502290f74197d18a3c4e16977782d39ee
Samdal <samdal@protonmail.com>
2021-07-06 10:18:29 +0200

    gl_FragColor is deprecated

--- frag.glsl
+++ frag.glsl
@@ -1,6 +1,7 @@
 #version 330 core
 precision mediump float;
 
+layout(location = 0) out vec4 diffuseColor;
 uniform int u_map[32];
 uniform vec2 u_resolution;
 uniform vec2 u_food;
@@ -10,8 +11,8 @@ void main()
     vec2 st = (gl_FragCoord.xy / u_resolution.xy) * 31.0;
     int x = int(floor(st.x));
     int y = int(floor(st.y));
-    gl_FragColor.g = float(u_map[y] & (1 << x));
-    gl_FragColor.r = float(x == int(u_food.x) && y == int(u_food.y));
-    gl_FragColor.b = 0.0;
-    gl_FragColor.a = 1.0;
+    diffuseColor.g = float(u_map[y] & (1 << x));
+    diffuseColor.r = float(x == int(u_food.x) && y == int(u_food.y));
+    diffuseColor.b = 0.0;
+    diffuseColor.a = 1.0;
 }
